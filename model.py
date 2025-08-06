dfe06cfb0d6e9895154795380b8bcb164bbc6b6d
Charanlokesh22 <charanlokesh2918@gmail.com>
2025-08-06 20:23:51 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
+
+# Dummy training dataset
+data = {
+    'month': [1, 2, 3, 4, 5, 6],
+    'expenses': [4000, 4200, 4100, 4300, 3900, 4400],
+    'income': [7000, 7100, 7050, 7200, 6900, 7300]
+}
+df = pd.DataFrame(data)
+
+X = df[['month', 'income']]
+y = df['expenses']
+
+model = LinearRegression()
+model.fit(X, y)
+
+joblib.dump(model, 'budget_model.pkl')
