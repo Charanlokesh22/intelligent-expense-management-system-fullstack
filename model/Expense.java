b5e8b0fd0aff9a410c7ce06527995412683e7c9e
Charanlokesh22 <charanlokesh2918@gmail.com>
2025-08-06 20:28:25 +0530

    Create Expense.java

---
+++ model/Expense.java
@@ -0,0 +1,18 @@
+package com.expenseapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String category;
+    private Double amount;
+    private String date;
+}
